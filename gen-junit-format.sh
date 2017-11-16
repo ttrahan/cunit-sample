b0105d61822a44d8fde45f4ed4071a937efb6801
Tom Trahan <tom@shippable.com>
2017-11-16 10:30:05 -0800

    troubleshoot test reporting in UI

--- gen-junit-format.sh
+++ gen-junit-format.sh
@@ -4,7 +4,7 @@
 cat <<EOF > $SHIPPABLE_BUILD_DIR/shippable/testresults/test_results.xml
 <?xml version="1.0" ?>
 <testsuites>
-	<testsuite errors="4" failures="2" name="my test suite" skipped="0" tests="1" time="123.345">
+	<testsuite errors="4" failures="2" name="my test suite" skipped="0" tests="2" time="123.345">
 		<testcase classname="some.class.name" name="Test1" time="123.345000">
 			<system-out>I am stdout!</system-out>
 			<system-err>I am stderr!</system-err>
