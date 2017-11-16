68bc1585cc319dc030713329916daabb18060faa
Tom Trahan <tom@shippable.com>
2017-11-16 10:47:48 -0800

    troubleshoot test reporting in UI

--- gen-junit-format-junitv2_success.sh
+++ gen-junit-format-junitv2_success.sh
@@ -10,8 +10,10 @@ cat <<EOF > $SHIPPABLE_BUILD_DIR/shippable/testresults/test_results_junitv2.xml
 <?xml version="1.0" encoding="UTF-8"?>
 <testsuites>
   <testsuite name="Infor\Hiro\Tests\DIContainerTest" file="/home/shippable/workspace/src/github.com/vidyar/hiro/tests/DIContainerTest.php" tests="2" assertions="2" failures="0" errors="0" time="0.05">
+<!--
     <testcase name="testWithString" class="Infor\Hiro\Tests\DIContainerTest" file="/home/shippable/workspace/src/github.com/vidyar/hiro/tests/DIContainerTest.php" line="7" assertions="1" time="0.02"/>
     <testcase name="testWithClosure" class="Infor\Hiro\Tests\DIContainerTest" file="/home/shippable/workspace/src/github.com/vidyar/hiro/tests/DIContainerTest.php" line="15" assertions="1" time="0.03"/>
+-->
   </testsuite>
   <testsuite name="ServiceContainerTest" file="/home/shippable/workspace/src/github.com/vidyar/hiro/tests/ServiceContainerTest.php" tests="2" assertions="3" failures="0" errors="0" time="0.14">
     <testcase name="testWithArgument" class="ServiceContainerTest" file="/home/shippable/workspace/src/github.com/vidyar/hiro/tests/ServiceContainerTest.php" line="5" assertions="1" time="0.08"/>
